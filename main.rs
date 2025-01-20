7743d5bf9a41e5863a56b75c006a4d293fd576be
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-01-20 22:26:52 +0530

    Add tuples

--- main.rs
+++ main.rs
@@ -1,4 +1,23 @@
 fn main() {
-    let x:f64 = 1.3247f64;
+
+    // var bindings + type annotations + unused vars
+
+    let y:i32 = 123;
+    println!("{}", y);
+
+    // using x after first line only refers to second x
+    // first x still exists (until scope),
+    // can no longer refer to it
+    // (shadowing bindings)
+
+    let x = 10;
+    let x = x + 3;
+
     println!("{}", x);
+
+    // tuples (fixed length collections of values of diff types)
+    // with type annotation
+    
+    let pair: (char, i32) = ('a', 17);
+    println!("{:?}", pair);
 }
