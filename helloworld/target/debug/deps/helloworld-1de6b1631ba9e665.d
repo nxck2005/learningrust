6118ae521f1cd5369c8f900715fd30f3a9fd8c65
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-06-15 23:51:42 +0530

    learning...

---
+++ helloworld/target/debug/deps/helloworld-1de6b1631ba9e665.d
@@ -0,0 +1,5 @@
+/home/nick/projects/learningrust/helloworld/target/debug/deps/libhelloworld-1de6b1631ba9e665.rmeta: src/main.rs
+
+/home/nick/projects/learningrust/helloworld/target/debug/deps/helloworld-1de6b1631ba9e665.d: src/main.rs
+
+src/main.rs:
